23c50d67e96d99a0fe4d862d52218950f9978355
Peter Afif <pafif@csu.fullerton.edu>
2026-05-26 14:27:06 -0700

    style: polish header layout and spacing

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,15 +3,28 @@ import "./Header.scss";
 export default function Header() {
   return (
     <header className="header">
-      <input
-        className="header__search"
-        type="text"
-        placeholder="Search for anything"
-      />
+      <div className="header__search-wrapper">
+        <input
+          className="header__search"
+          type="text"
+          placeholder="Search for anything"
+        />
+        <button className="header__search-btn">
+          <img src="/icons/search.svg" alt="search" />
+        </button>
+      </div>
       <div className="header__right">
         <a href="#">Docs</a>
-        <img src="/icons/notifications.svg" alt="notifications" />
-        <span>Adedeji</span>
+        <img
+          src="/icons/notifications.svg"
+          alt="notifications"
+          className="header__bell"
+        />
+        <div className="header__user">
+          <img src="/avatar.png" alt="Adedeji" className="header__avatar" />
+          <span className="header__username">Adedeji</span>
+          <img src="/icons/dropdown.svg" alt="" />
+        </div>
       </div>
     </header>
   );
